ebe6d55755b1a5433560401167c2540cbb29fabc
wdnmd030 <85606797+wdnmd030@users.noreply.github.com>
2022-08-17 22:49:12 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -17,7 +17,7 @@ module.exports = {
   kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "Spencer's OneDrive",
+  title: "萝莉的小屋",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/0000/',
